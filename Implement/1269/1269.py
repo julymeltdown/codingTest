20638c8967f87240cf4a7dbc16c971e3a2f4fcf7
Julymeltdown <quintuplets2000@gmail.com>
2022-09-19 13:55:30 +0900

    1269

---
+++ Implement/1269/1269.py
@@ -0,0 +1,8 @@
+import sys
+
+ALen, BLen = map(int, sys.stdin.readline().split(" "))
+A = set(map(int, sys.stdin.readline().split(" ")))
+B = set(map(int, sys.stdin.readline().split(" ")))
+
+
+print(len(list(A - B)) + len(list(B - A)))
